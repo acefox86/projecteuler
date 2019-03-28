2a365466e1731ea6cd2e4eacc46ec94afdfb6026
Owen Fernau <11252046+owenfernau@users.noreply.github.com>
2019-03-27 20:23:00 -0700

    Update quantecon.py

--- quantecon.py
+++ quantecon.py
@@ -1,4 +1,11 @@
 # problems via this site https://lectures.quantecon.org/py/
 # also going to write some notes as I go.
 
-# zip() creates dictionaries 
+#python is a small language with lots of libraries 
+
+#important libraries
+#matplotlib
+#numpy
+#scipy - built on numpy
+#sympy - turns code into algebra
+#pandas
