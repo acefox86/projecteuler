a97c55c18f3e6f04a9133b4b63470b64d4e56fa4
Owen Fernau <11252046+owenfernau@users.noreply.github.com>
2019-03-31 07:59:06 -0700

    Update quantecon.py

--- quantecon.py
+++ quantecon.py
@@ -1,7 +1,9 @@
 # problems via this site https://lectures.quantecon.org/py/
 # also going to write some notes as I go.
 
-#python is a small language with lots of libraries 
+#python is a small language with lots of packages
+#write your own user-defined functions to make your intentions clearer
+#remember you can pass functions to functions
 
 #important libraries
 #matplotlib
@@ -9,3 +11,5 @@
 #scipy - built on numpy
 #sympy - turns code into algebra
 #pandas
+
+
